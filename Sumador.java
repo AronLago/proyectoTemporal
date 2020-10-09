a1541ae975dd524bea9750a14bfbe3b8a838c564
Aron Lago <aironlago@gmail.com>
2020-10-09 11:00:01 +0200

    proyecto inicial

---
+++ Sumador.java
@@ -0,0 +1,9 @@
+public class Sumador{
+    public static void main(String[] args) {
+        int num1=3;
+        int num2=2;
+        int suma= num1+num2;
+
+        System.out.println("Lasumavale "+suma);
+    }
+}
